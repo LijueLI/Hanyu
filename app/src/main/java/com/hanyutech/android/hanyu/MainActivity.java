24a548d88139462ca621c99754985307631b5ace
LijueLI <32730248+LijueLI@users.noreply.github.com>
2018-07-12 11:35:50 +0800

    稍微改善目前UI介面並增添一活動

--- app/src/main/java/com/hanyutech/android/hanyu/MainActivity.java
+++ app/src/main/java/com/hanyutech/android/hanyu/MainActivity.java
@@ -1,13 +1,31 @@
 package com.hanyutech.android.hanyu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button Enterinfo;
+    private Intent EnterinfoAct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN); //將螢幕設為全螢幕並且隱藏title
+
+        Enterinfo = findViewById(R.id.EnterInfo);
+        EnterinfoAct = new Intent();
+        EnterinfoAct.setClass(this,Enterinfo.class);
+        Enterinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(EnterinfoAct);
+            }
+        });
     }
 }
